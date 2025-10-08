144480d311c062589ea6e78f666c528574ba9fcb
EmilySouza22 <emilyseafonso@gmail.com>
2025-10-08 17:01:52 -0300

    Add pequenas alterações no código da aula.

--- 11-router/src/pages/Home.jsx
+++ 11-router/src/pages/Home.jsx
@@ -7,10 +7,10 @@ function Home() {
 			<Navbar />
 			<h1>Home</h1>
 
-			<input type="text" />
+			{/* <input type="text" />
 			<div>
 				<p>exemplo</p>
-			</div>
+			</div> */}
 		</div>
 	);
 }
--- 11-router/src/pages/Produtos.jsx
+++ 11-router/src/pages/Produtos.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar'
 import { GlobalContext } from '../contexts/GlobalContext'
 
 function Produtos() {
-    const { produto, produtos } = useContext(GlobalContext)
+    const { produto, setProduto, produtos } = useContext(GlobalContext)
 
     function trocarProduto(){
         let novo = prompt('Novo nome:')
@@ -16,7 +16,7 @@ function Produtos() {
         <Navbar/>
         <h1>Produtos</h1>
         produtos, produtos, produtos, {produto}, {produtos[0].nome}, {produtos[1].nome}
-        <button onClick={trocarProduto}></button>
+        <button onClick={trocarProduto}>Trocar</button>
     </div>
   )
 }
